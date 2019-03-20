c055aa66a2be2889b1981c3b58de4a41fbcf057a
jaskaranbhatia <jaskaransingh.singh77@gmail.com>
2019-03-20 23:08:14 +0530

    Updates

---
+++ precache-manifest.74f82cca967b7ed63bbca577cb924fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "112eb2a9bc0338f97a28",
+    "url": "/ContactManager/static/js/runtime~main.0e09fb07.js"
+  },
+  {
+    "revision": "85816040938ac680ada2",
+    "url": "/ContactManager/static/js/main.97848a95.chunk.js"
+  },
+  {
+    "revision": "2aa28398534485c5f64b",
+    "url": "/ContactManager/static/js/2.77a9f17a.chunk.js"
+  },
+  {
+    "revision": "85816040938ac680ada2",
+    "url": "/ContactManager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "2aa28398534485c5f64b",
+    "url": "/ContactManager/static/css/2.e2da55c8.chunk.css"
+  },
+  {
+    "revision": "03a4d2b5f6e6d7b947ea09410703d6b1",
+    "url": "/ContactManager/index.html"
+  }
+];
